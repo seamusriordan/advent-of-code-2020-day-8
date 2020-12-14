9ea76c304faf3c3a475fcb10948ff18d53f1898e
Seamus Riordan <seamus@riordan.io>
2020-12-14 06:16:22 -0600

    Sum accumulations

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,13 @@
+import Instruction.Operation.*
+
 fun main() {
     println("Hello World!")
+}
+
+fun runBootCode(instructions: List<Instruction>): Int {
+    return instructions.filter {
+        it.operation == ACC
+    }.map {
+        it.value
+    }.sum()
 }
--- src/test/kotlin/MainTests.kt
+++ src/test/kotlin/MainTests.kt
@@ -1,9 +1,68 @@
+import Instruction.Operation.*
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class MainTests {
     @Test
-    internal fun something() {
-        assertEquals(true, true)
+    internal fun `no instructions does nothing`() {
+        val result = runBootCode(listOf())
+
+        assertEquals(0, result)
+    }
+
+    @Test
+    internal fun `one instruction nop 0 does nothing`() {
+
+        val instructions = listOf(
+            Instruction(NOP, 0)
+        )
+        val result = runBootCode(instructions)
+
+        assertEquals(0, result)
+    }
+
+    @Test
+    internal fun `instruction acc 0 does nothing`() {
+
+        val instructions = listOf(
+            Instruction(ACC, 0)
+        )
+        val result = runBootCode(instructions)
+
+        assertEquals(0, result)
+    }
+
+    @Test
+    internal fun `instruction jmp 1 does nothing`() {
+
+        val instructions = listOf(
+            Instruction(JMP, 1)
+        )
+        val result = runBootCode(instructions)
+
+        assertEquals(0, result)
+    }
+
+    @Test
+    internal fun `instruction acc 1 returns 1`() {
+
+        val instructions = listOf(
+            Instruction(ACC, 1)
+        )
+        val result = runBootCode(instructions)
+
+        assertEquals(1, result)
+    }
+
+    @Test
+    internal fun `two instructions acc 2 returns 2`() {
+
+        val instructions = listOf(
+            Instruction(ACC, 1),
+            Instruction(ACC, 1)
+        )
+        val result = runBootCode(instructions)
+
+        assertEquals(2, result)
     }
 }
